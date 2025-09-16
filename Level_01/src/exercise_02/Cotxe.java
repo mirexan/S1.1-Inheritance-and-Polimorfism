c1e4ee94678402c085a89b7ef21777ab0c5c9b37
mirexan <mmregada@gmail.com>
2025-09-16 11:16:51 +0200

    quito comentarios

--- Level_01/src/exercise_02/Cotxe.java
+++ Level_01/src/exercise_02/Cotxe.java
@@ -8,14 +8,6 @@ public class Cotxe {
     private static String model;
     private final int potencia;
 
-    /* ·static: label means that this variable belongs to "Cotxe" class, not just
-    *   the object to instance. There's only one space in the memory who is gone to be
-    *   shared with all "Cotxes" instances.
-    * ·final: means the variable it's a constant, once assigned it can't be modified.
-    * So, marca it´s going to be initialized in attributes directly because its static and final, so it
-    * cannot be modified in every instance.
-    * model can be assigned in constructor.*/
-
     //CONSTRUCTOR
 
     public Cotxe(int potencia)
